7e0f95397967dad67f2edd8c425250eb1e636bca
Aat <karelseaat@gmail.com>
2022-01-10 16:53:49 +0100

    more emails send

--- getgoodemails.py
+++ getgoodemails.py
@@ -16,7 +16,7 @@ dbsession = make_session()
 now = datetime.datetime.now()
 sixmonthsago = now - datetime.timedelta(days=128)
 
-result = dbsession.query(App).filter(App.installs > 50).filter(App.installs < 500).filter(App.lastupdate > sixmonthsago).distinct(App.email).all()
+result = dbsession.query(App).filter(App.installs > 50).filter(App.id > 11493).filter(App.installs < 500).filter(App.lastupdate > sixmonthsago).distinct(App.email).all()
 
 for res in result:
     print(res.email, res.appid)
